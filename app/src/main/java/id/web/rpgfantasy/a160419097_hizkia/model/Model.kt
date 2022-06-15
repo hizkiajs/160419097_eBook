a2f34a40a8e65e51b1780570d2dda99f60ba3e74
hizkiajs <hizkiajs17@gmail.com>
2022-06-15 18:42:03 +0700

    Profile, EBook Room

--- app/src/main/java/id/web/rpgfantasy/a160419097_hizkia/model/Model.kt
+++ app/src/main/java/id/web/rpgfantasy/a160419097_hizkia/model/Model.kt
@@ -1,21 +1,44 @@
 package id.web.rpgfantasy.a160419097_hizkia.model
 
 import com.google.gson.annotations.SerializedName
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-data class Profile(var id: String?,
-                   var nama: String?,
-                   var tanggalLahir: String?,
-                   var jenisKelamin: String?,
-                   var email: String?,
-                   var noHP: String?,
-                   var photo: String?)
+@Entity
+data class Profile(
+    @ColumnInfo(name = "id")
+    var id: String?,
+    @ColumnInfo(name = "nama")
+    var nama: String?,
+    @ColumnInfo(name = "tanggalLahir")
+    var tanggalLahir: String?,
+    @ColumnInfo(name = "jenisKelamin")
+    var jenisKelamin: String?,
+    @ColumnInfo(name = "email")
+    var email: String?,
+    @ColumnInfo(name = "noHP")
+    var noHP: String?,
+    @ColumnInfo(name = "photo")
+    var photo: String?)
 
-data class EBook(var id: String?,
-                 var nama: String?,
-                 var editor: String?,
-                 var publish: String?,
-                 var copyright: String?,
-                 var about: String?,
-                 var author_bios: String?,
-                 var review: String?,
-                 var photo: String?)
+@Entity
+data class EBook(
+    @ColumnInfo(name = "id")
+    var id: String?,
+    @ColumnInfo(name = "nama")
+    var nama: String?,
+    @ColumnInfo(name = "editor")
+    var editor: String?,
+    @ColumnInfo(name = "publish")
+    var publish: String?,
+    @ColumnInfo(name = "copyright")
+    var copyright: String?,
+    @ColumnInfo(name = "about")
+    var about: String?,
+    @ColumnInfo(name = "author_bios")
+    var author_bios: String?,
+    @ColumnInfo(name = "review")
+    var review: String?,
+    @ColumnInfo(name = "photo")
+    var photo: String?)
