3edd5ee9367e968a54274579e65cef12aa0f01b6
hizkiajs <hizkiajs17@gmail.com>
2022-06-17 15:45:27 +0700

    ELibDao done

--- app/src/main/java/id/web/rpgfantasy/a160419097_hizkia/model/ELibDao.kt
+++ app/src/main/java/id/web/rpgfantasy/a160419097_hizkia/model/ELibDao.kt
@@ -2,6 +2,7 @@ package id.web.rpgfantasy.a160419097_hizkia.model
 
 import androidx.room.*
 
+@Dao
 interface EbookDao {
     @Query("SELECT * FROM Ebook")
     suspend fun selectAllEbook():List<EBook>
@@ -17,10 +18,39 @@ interface EbookDao {
 
     @Update
     suspend fun updateEbook(eBook: EBook)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(vararg eBook:EBook)
 }
 interface ProfileDao{
+    @Query("SELECT * FROM Profile")
+    suspend fun selectProfile():List<Profile>
 
+    @Delete
+    suspend fun deleteProfile(profile: Profile)
+
+    @Query("UPDATE Profile SET nama = :name, tanggalLahir = :tanggalLahir, jenisKelamin = :jenisKelamin, email=:email, noHP=:noHP, photo=:photo, photo=:photo WHERE id =:idProfile")
+    suspend fun update(name:String, tanggalLahir:String, jenisKelamin:String, email:String, noHP:String, photo:String, idProfile:Int )
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(vararg profile:Profile)
 }
 interface CategoryDao{
+    @Query("SELECT * FROM Category")
+    suspend fun selectAllCategory():List<Category>
 
+    @Query("SELECT * FROM Category WHERE id = :id")
+    suspend fun selectCategoryById(id:Int):List<Category>
+
+    @Delete
+    suspend fun deleteCategory(category: Category)
+
+    @Query("UPDATE Category SET nama = :nama WHERE id =:idkategori")
+    suspend fun update(nama:String, idkategori: Int )
+
+    @Update
+    suspend fun updateCategory(category: Category)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(vararg category:Category)
 }
