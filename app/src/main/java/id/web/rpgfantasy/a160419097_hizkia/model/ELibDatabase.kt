4b4115d25213a24aeb6dd1279d926ae81cc5f74b
Gilbert <s160419105@student.ubaya.ac.id>
2022-07-01 00:33:18 +0700

    Done Fix?

--- app/src/main/java/id/web/rpgfantasy/a160419097_hizkia/model/ELibDatabase.kt
+++ app/src/main/java/id/web/rpgfantasy/a160419097_hizkia/model/ELibDatabase.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import id.web.rpgfantasy.a160419097_hizkia.util.MIGRATION_1_2
 
 
 @Database(entities = [Profile::class,Category::class, EBook::class], version = 2)
@@ -18,7 +20,7 @@ abstract class ELibDatabase:RoomDatabase() {
 
         private fun buildDatabase(context: Context) =
             Room.databaseBuilder(context.applicationContext,ELibDatabase::class.java,"ELibDatabase")
-                .addMigrations()
+                .addMigrations(MIGRATION_1_2)
                 .build()
 
         operator fun invoke(context: Context){
